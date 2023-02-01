e2d528df9d5c36b4c1df71a7c871e1d41cee27d2
Anton Trafimovich <antont@compatibl.com>
2023-02-01 19:10:38 +0100

    feat: add album repo.

---
+++ src/repository/album.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { DB } from '../db/db.model';
+
+import { Album } from '../model';
+
+@Injectable()
+export class AlbumRepository {
+  private _segment = 'album';
+
+  constructor(private _db: DB<Album>) {}
+
+  getAll(): Promise<Album[]> {
+    return this._db.getMany({
+      segment: this._segment,
+    });
+  }
+
+  getById(id: string): Promise<Album> {
+    return this._db.getOne({
+      segment: this._segment,
+      fn: (album) => {
+        return album.id === id;
+      },
+    });
+  }
+
+  create(album: Omit<Album, 'id'>): Promise<Album> {
+    return this._db.create({
+      segment: this._segment,
+      payload: album,
+    });
+  }
+
+  update(id: string, album: Partial<Omit<Album, 'id'>>): Promise<Album> {
+    return this._db.update({
+      segment: this._segment,
+      fn: (album) => {
+        return album.id === id;
+      },
+      payload: album,
+    });
+  }
+
+  delete(id: string): Promise<void> {
+    return this._db.delete({
+      segment: this._segment,
+      fn: (album) => {
+        return album.id === id;
+      },
+    });
+  }
+}
--- src/repository/repository.module.ts
+++ src/repository/repository.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { AlbumRepository } from './album.repository';
 import { ArtistRepository } from './artist.repository';
 import { TrackRepository } from './track.repository';
 import { UserRepository } from './user.repository';
 
 @Module({
   imports: [],
-  providers: [UserRepository, TrackRepository, ArtistRepository],
-  exports: [UserRepository, TrackRepository, ArtistRepository],
+  providers: [
+    UserRepository,
+    TrackRepository,
+    ArtistRepository,
+    AlbumRepository,
+  ],
+  exports: [UserRepository, TrackRepository, ArtistRepository, AlbumRepository],
 })
 export class RepositoryModule {}
