be0e8fe9f1193aaf4d6a2652b5f449cc6b726b79
Anton Trafimovich <antont@compatibl.com>
2023-02-01 19:16:34 +0100

    feat: add album controller.

---
+++ src/album/album.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { Album } from '../model';
+import { CreateAlbumDTO, UpdateAlbumDTO } from './album.model';
+import { AlbumService } from './album.service';
+
+@Controller('Album')
+export class AlbumController {
+  constructor(private _albumService: AlbumService) {}
+
+  @Get()
+  getAll(): Promise<Album[]> {
+    return this._albumService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string): Promise<Album> {
+    console.log(id);
+    return this._albumService.getById(id);
+  }
+
+  @Post()
+  create(@Body() createAlbumDTO: CreateAlbumDTO): Promise<Album> {
+    console.log(createAlbumDTO);
+    return this._albumService.create(createAlbumDTO);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id') id: string,
+    @Body() updateAlbumDTO: UpdateAlbumDTO,
+  ): Promise<Album> {
+    return this._albumService.update(id, updateAlbumDTO);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this._albumService.delete(id);
+  }
+}
