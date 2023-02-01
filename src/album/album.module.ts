f8d074616f63ee63c59315cb85b88c8fec6fee0d
Anton Trafimovich <antont@compatibl.com>
2023-02-01 19:17:50 +0100

    feat: add album module.

---
+++ src/album/album.module.ts
@@ -0,0 +1,10 @@
+import { AlbumService } from './album.service';
+import { AlbumController } from './album.controller';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [AlbumController],
+  providers: [AlbumService],
+})
+export class AlbumModule {}
