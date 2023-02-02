2ab553e59f705a5637c0231fcb0e903392cad359
Anton Trafimovich <antont@compatibl.com>
2023-02-02 19:35:58 +0100

    feat: add rest of modules to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
 import { DBModule } from './db/db.module';
+import { FavoritesModule } from './favorites/favorites.module';
 import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DBModule.forRoot({ type: 'inMemory' }), TrackModule, UserModule],
+  imports: [
+    DBModule.forRoot({ type: 'inMemory' }),
+    TrackModule,
+    UserModule,
+    ArtistModule,
+    FavoritesModule,
+    AlbumModule,
+  ],
 })
 export class AppModule {}
