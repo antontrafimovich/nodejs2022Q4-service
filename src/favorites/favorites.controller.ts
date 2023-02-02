9659bea7652e35ab6f1ac39d80f78c030abcc882
Anton Trafimovich <antont@compatibl.com>
2023-02-02 19:05:11 +0100

    feat: add favorite controller.

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,14 +1,86 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { FullfieldFavorites } from './favorites.model';
 import { FavoritesService } from './favorites.service';
 
 @Controller('favs')
 export class FavoritesController {
-  constructor(private _favouriteService: FavoritesService) {}
+  constructor(private _favoriteService: FavoritesService) {}
 
   @Get()
   getAll(): Promise<FullfieldFavorites> {
-    return this._favouriteService.getAll();
+    return this._favoriteService.getAll();
+  }
+
+  @Post('track/:id')
+  async addTrackToFavorites(@Param('id') trackId: string): Promise<string> {
+    try {
+      await this._favoriteService.addTrackToFavorites(trackId);
+    } catch (err) {
+      throw err;
+    }
+
+    return 'Success';
+  }
+
+  @Delete('track/:id')
+  async deleteTrackFromFavorites(
+    @Param('id') trackId: string,
+  ): Promise<string> {
+    try {
+      await this._favoriteService.deleteTrackFromFavorites(trackId);
+    } catch (err) {
+      throw err;
+    }
+
+    return 'Success';
+  }
+
+  @Post('album/:id')
+  async addAlbumToFavorites(@Param('id') trackId: string): Promise<string> {
+    try {
+      await this._favoriteService.addTrackToFavorites(trackId);
+    } catch (err) {
+      throw err;
+    }
+
+    return 'Success';
+  }
+
+  @Delete('album/:id')
+  async deleteAlbumFromFavorites(
+    @Param('id') albumId: string,
+  ): Promise<string> {
+    try {
+      await this._favoriteService.deleteAlbumFromFavorites(albumId);
+    } catch (err) {
+      throw err;
+    }
+
+    return 'Success';
+  }
+
+  @Post('artist/:id')
+  async addArtistToFavorites(@Param('id') artistId: string): Promise<string> {
+    try {
+      await this._favoriteService.addArtistToFavorites(artistId);
+    } catch (err) {
+      throw err;
+    }
+
+    return 'Success';
+  }
+
+  @Delete('artist/:id')
+  async deleteArtistFromFavorites(
+    @Param('id') artistId: string,
+  ): Promise<string> {
+    try {
+      await this._favoriteService.deleteArtistFromFavorites(artistId);
+    } catch (err) {
+      throw err;
+    }
+
+    return 'Success';
   }
 }
