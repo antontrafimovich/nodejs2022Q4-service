9bb5d6f39b3ff0d55442e66e10903d526e65f2a3
Anton Trafimovich <antont@compatibl.com>
2023-02-04 13:12:34 +0100

    feat: add ability to remove album from track and favorites if it's being deleted.

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,16 +1,17 @@
-import {
-  HttpCode,
-  HttpException,
-  HttpStatus,
-  Injectable,
-} from '@nestjs/common';
-import { AlbumRepository } from 'src/repository/album.repository';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { FavoritesRepository } from 'src/repository/favorites.repository';
 
-import { Album } from '../model';
+import { Album, Track } from '../model';
+import { AlbumRepository } from '../repository/album.repository';
+import { TrackRepository } from '../repository/track.repository';
 
 @Injectable()
 export class AlbumService {
-  constructor(private _albumRepo: AlbumRepository) {}
+  constructor(
+    private _albumRepo: AlbumRepository,
+    private _trackRepo: TrackRepository,
+    private _favoritesRepo: FavoritesRepository,
+  ) {}
 
   getAll(): Promise<Album[]> {
     return this._albumRepo.getAll();
@@ -41,7 +42,30 @@ export class AlbumService {
 
   async delete(albumId: string): Promise<void> {
     try {
-      return await this._albumRepo.delete(albumId);
+      await this._albumRepo.delete(albumId);
+    } catch ({ message }) {
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      await this._favoritesRepo.delete({ type: 'album', entityId: albumId });
+    } catch {}
+
+    let albumTracks: Track[];
+    try {
+      albumTracks = await this._trackRepo.getMany(
+        (track) => track.albumId === albumId,
+      );
+    } catch ({ message }) {
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      const updateTracksPromises = albumTracks.map((track) =>
+        this._trackRepo.update(track.id, { albumId: null }),
+      );
+
+      await Promise.all(updateTracksPromises);
     } catch ({ message }) {
       throw new HttpException(message, HttpStatus.NOT_FOUND);
     }
