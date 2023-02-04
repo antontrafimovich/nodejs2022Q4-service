435a1f1e7069b20fd6586e730f8e0344b93dd06b
Anton Trafimovich <antont@compatibl.com>
2023-02-04 13:13:06 +0100

    feat: add ability to remove artist from tracks, albums and favorites if it's being deleted.

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,11 +1,19 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { ArtistRepository } from '../repository/artist.repository';
+import { FavoritesRepository } from 'src/repository/favorites.repository';
 
-import { Artist } from '../model';
+import { Album, Artist, Track } from '../model';
+import { AlbumRepository } from '../repository/album.repository';
+import { ArtistRepository } from '../repository/artist.repository';
+import { TrackRepository } from '../repository/track.repository';
 
 @Injectable()
 export class ArtistService {
-  constructor(private _artistRepo: ArtistRepository) {}
+  constructor(
+    private _artistRepo: ArtistRepository,
+    private _trackRepo: TrackRepository,
+    private _albumRepo: AlbumRepository,
+    private _favoritesRepo: FavoritesRepository,
+  ) {}
 
   getAll(): Promise<Artist[]> {
     return this._artistRepo.getAll();
@@ -37,6 +45,50 @@ export class ArtistService {
   async delete(artistId: string): Promise<void> {
     try {
       await this._artistRepo.delete(artistId);
+    } catch ({ message }) {
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      await this._favoritesRepo.delete({ type: 'artist', entityId: artistId });
+    } catch {}
+
+    let artistTracks: Track[];
+
+    try {
+      artistTracks = await this._trackRepo.getMany(
+        (track) => track.artistId === artistId,
+      );
+    } catch ({ message }) {
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      const updateTracksPromises = artistTracks.map((track) =>
+        this._trackRepo.update(track.id, { artistId: null }),
+      );
+
+      await Promise.all(updateTracksPromises);
+    } catch ({ message }) {
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    }
+
+    let artistAlbums: Album[];
+
+    try {
+      artistAlbums = await this._albumRepo.getMany(
+        (album) => album.artistId === artistId,
+      );
+    } catch ({ message }) {
+      throw new HttpException(message, HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      const updateAlbumsPromises = artistAlbums.map((album) =>
+        this._albumRepo.update(album.id, { artistId: null }),
+      );
+
+      await Promise.all(updateAlbumsPromises);
     } catch ({ message }) {
       throw new HttpException(message, HttpStatus.NOT_FOUND);
     }
