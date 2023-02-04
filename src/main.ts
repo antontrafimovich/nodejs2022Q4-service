c7568068dedfd80ac230a14967b746e1273a77fb
Anton Trafimovich <antont@compatibl.com>
2023-02-04 14:42:50 +0100

    feat: add support for .env PORT.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(4000);
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
 }
 bootstrap();
