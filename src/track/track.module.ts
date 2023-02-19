47dfc76bb44f97847760bb0d49e986ce6f8937e4
Anton Trafimovich <antont@compatibl.com>
2023-02-19 17:07:29 +0100

    fix: get rid of unused module in track.module.

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -3,20 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AlbumEntity } from '../album/entity/album.entity';
 import { ArtistEntity } from '../artist/entity/artist.entity';
-import { FavoriteEntity } from '../favorites/entity/favorite.entity';
 import { TrackEntity } from './entity/track.entity';
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      ArtistEntity,
-      AlbumEntity,
-      TrackEntity,
-      FavoriteEntity,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([ArtistEntity, AlbumEntity, TrackEntity])],
   controllers: [TrackController],
   providers: [TrackService],
 })
