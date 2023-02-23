5b60974cbc43154a7b73574a57b8fae54e156276
Anton Trafimovich <antont@compatibl.com>
2023-02-23 12:20:39 +0100

    feat: add ability to generate accessToken on login.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 import { User } from '../model';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   validateUser(
     login: string,
@@ -15,5 +19,16 @@ export class AuthService {
       login,
       password,
     });
+  }
+
+  async login(user: User) {
+    const payload = {
+      sub: user.id,
+      login: user.login,
+    };
+
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
   }
 }
