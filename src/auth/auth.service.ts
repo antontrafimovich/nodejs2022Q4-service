6b0d54cd0bb017e9d505ad05f1132838065a6e87
Anton Trafimovich <antont@compatibl.com>
2023-02-25 15:06:28 +0100

    fix: change claim name in jwt token.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
 
   async login(user: User): Promise<AuthLoginResult> {
     const payload = {
-      sub: user.id,
+      userId: user.id,
       login: user.login,
     };
 
