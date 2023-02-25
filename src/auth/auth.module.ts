a1002896fbe539195f9cdade5f88a8f78c0f583a
Anton Trafimovich <antont@compatibl.com>
2023-02-25 14:42:45 +0100

    feat: register jwt guard and strategy.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,38 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport/dist';
 
 import { UserModule } from '../user/user.module';
-import { LocalStrategy } from './auth-strategies/local.strategy';
+import { JWT_CONSTANTS } from './auth.constants';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
-import { UserExistsGuard } from './user-exists.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({
-      secret: 'random secret',
+      secret: JWT_CONSTANTS.secret,
       signOptions: {
         expiresIn: '2m',
       },
     }),
   ],
-  providers: [AuthService, LocalStrategy, LocalAuthGuard, UserExistsGuard],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    LocalAuthGuard,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
