5304b3c887ec1deee8213e632abce43368a7ab9f
Anton Trafimovich <antont@compatibl.com>
2023-02-26 20:57:50 +0100

    fix: move log logic from logging interceptor to log service.

--- src/logger/logging.interceptor.ts
+++ src/logger/logging.interceptor.ts
@@ -1,11 +1,12 @@
 import {
+  CallHandler,
+  ExecutionContext,
   Injectable,
   NestInterceptor,
-  ExecutionContext,
-  CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+
 import { LoggingSerivce } from './logging.service';
 
 @Injectable()
@@ -13,41 +14,13 @@ export class LoggingInterceptor implements NestInterceptor {
   constructor(private loggingService: LoggingSerivce) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const { url, query, body } = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest();
 
-    console.log(context);
-
-    const urlString = `url: ${url}`;
-
-    const queryString = Object.keys(query).reduce((result, next) => {
-      if (result === null) {
-        return `query: ${next}=${query[next]}`;
-      }
-
-      return `${result} ${next}=${query[next]}`;
-    }, null);
-
-    const bodyString = Object.keys(body).reduce((result, next) => {
-      if (result === null) {
-        return `body: ${next}=${body[next]}`;
-      }
-
-      return `${result} ${next}=${body[next]}`;
-    }, null);
-
-    const resultString = [urlString, queryString, bodyString]
-      .filter((v) => v !== null)
-      .join('; ');
-
-    this.loggingService.log(resultString);
+    this.loggingService.logRequestData(request);
 
     return next.handle().pipe(
       tap({
         next: (data) => console.log(data),
-        error: (error) => {
-          this.loggingService.error(error.message);
-          console.log(error);
-        },
       }),
     );
   }
