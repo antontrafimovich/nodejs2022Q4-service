9e0a2f89728b131a01a1dc7d8f3ffc8734bfbf57
Anton Trafimovich <antont@compatibl.com>
2023-02-26 20:20:00 +0100

    feat: add logger module.

---
+++ src/logger/logger.module.ts
@@ -0,0 +1,17 @@
+import { Global, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+import { LoggingInterceptor } from './logging.interceptor';
+import { LoggingSerivce } from './logging.service';
+
+@Global()
+@Module({
+  providers: [
+    LoggingSerivce,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
+})
+export class LoggerModule {}
