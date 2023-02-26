0d7be6aa09688ce405522b41ff188c2a8844dc48
Anton Trafimovich <antont@compatibl.com>
2023-02-26 20:55:34 +0100

    feat: add logRequestData method to custom logger.

--- src/logger/logging.service.ts
+++ src/logger/logging.service.ts
@@ -1,4 +1,32 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LoggingSerivce extends ConsoleLogger {}
+export class LoggingSerivce extends ConsoleLogger {
+  public logRequestData(req: any) {
+    const { url, query, body } = req;
+
+    const urlString = `url: ${url}`;
+
+    const queryString = Object.keys(query).reduce((result, next) => {
+      if (result === null) {
+        return `query: ${next}=${query[next]}`;
+      }
+
+      return `${result} ${next}=${query[next]}`;
+    }, null);
+
+    const bodyString = Object.keys(body).reduce((result, next) => {
+      if (result === null) {
+        return `body: ${next}=${body[next]}`;
+      }
+
+      return `${result} ${next}=${body[next]}`;
+    }, null);
+
+    const resultString = [urlString, queryString, bodyString]
+      .filter((v) => v !== null)
+      .join('; ');
+
+    this.log(resultString);
+  }
+}
